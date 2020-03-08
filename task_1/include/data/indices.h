e8efa4b55f448c7981caaf88e3dbc78a397423e4
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-08 10:34:47 +0300

    create 3d cube

--- task_1/include/data/indices.h
+++ task_1/include/data/indices.h
@@ -2,8 +2,19 @@
 #define INDICES
 
 unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
+        0, 1, 2, 
+        3, 4, 5, 
+        6, 7, 8, 
+        9, 10, 11, 
+        12, 13, 14, 
+        15, 16, 17, 
+        18, 19, 20, 
+        21, 22, 23, 
+        24, 25, 26,
+        27, 28, 29,
+        30, 31, 32,
+        33, 34, 35
+
 };
 
 
--- task_1/src/task_1.cpp
+++ task_1/src/task_1.cpp
@@ -18,37 +18,20 @@
 
 
 void render_image(uint32_t shader_program, uint32_t buffer) {
-    float vertices[] = {
-         0.5f,  0.5f, 0.0f, // top right
-         0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f, // top left 
-    };  
-
     glUseProgram(shader_program);
+    glEnable(GL_DEPTH_TEST);
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glm::mat4 model(1.f);
     glm::mat4 view(1.f);
     glm::mat4 projection(1.f);
 
-    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(0.5f, 1.0f, 0.0f));
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
     projection = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f);
-
-    glm::mat4 some = projection * view * model;
-
-    std::cerr << "MODEL: " << std::endl << glm::to_string(model) << std::endl;
-    std::cerr << "VIEW: "  << std::endl << glm::to_string(view) << std::endl;
-    std::cerr << "PROJ: "  << std::endl << glm::to_string(projection) << std::endl;
-    std::cerr << "MULT: "  << std::endl << glm::to_string(some) << std::endl;
-   
-    std::cerr << "VERTICES: " << std::endl;
-    for (int i = 0; i < 4; i++) {
-        std::cerr << glm::to_string(model * glm::vec4(vertices[i*3], vertices[i*3 + 1], vertices[i*3 + 2], 1.0f)) << std::endl;
-    }
+    
 
     GLint model_loc = glGetUniformLocation(shader_program, "model");
     GLint view_loc = glGetUniformLocation(shader_program, "view");
@@ -61,7 +44,7 @@ void render_image(uint32_t shader_program, uint32_t buffer) {
 
     // draw triangle
     glBindVertexArray(buffer);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 }
 
