c35282eb6a60b4785fadab36cdc3f7e5b8df459d
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-08 10:36:10 +0300

    update image resolution

--- task_1/include/libs/init_opengl.h
+++ task_1/include/libs/init_opengl.h
@@ -2,8 +2,8 @@
 #ifndef INIT_OPENGL
 #define INIT_OPENGL
 
-const unsigned int SCR_WIDTH = 512;
-const unsigned int SCR_HEIGHT = 512;
+const unsigned int SCR_WIDTH = 1024;
+const unsigned int SCR_HEIGHT = 1024;
 
 GLFWwindow *init();
 
